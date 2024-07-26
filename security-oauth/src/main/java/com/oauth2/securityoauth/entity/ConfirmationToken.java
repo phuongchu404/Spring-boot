c9edd8f02985883d79a91bdd756e8445aed246b0
Phuong-Chu <phuongchu404@gmail.com>
2024-07-26 17:35:36 +0700

    update confirm by email

---
+++ security-oauth/src/main/java/com/oauth2/securityoauth/entity/ConfirmationToken.java
@@ -0,0 +1,39 @@
+package com.oauth2.securityoauth.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "confirmation_token")
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @MapsId
+    @JoinColumn(name = "id", nullable = false)
+    private User user;
+
+    public ConfirmationToken(User user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+
+
+}
